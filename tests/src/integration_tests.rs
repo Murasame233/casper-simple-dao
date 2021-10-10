c325592e252b935b1a9ea04e6cd34c512fa5c00b
Murasame <me@murasame.moe>
2021-10-10 17:04:05 +0800

    ADD: test

--- tests/src/integration_tests.rs
+++ tests/src/integration_tests.rs
@@ -1,67 +1,90 @@
 #[cfg(test)]
 mod tests {
-    use casper_engine_test_support::{Code, Error, SessionBuilder, TestContextBuilder, Value};
-    use casper_types::{
-        account::AccountHash, runtime_args, PublicKey, RuntimeArgs, SecretKey, U512,
-    };
 
-    const MY_ACCOUNT: [u8; 32] = [7u8; 32];
-    // Define `KEY` constant to match that in the contract.
-    const KEY: &str = "my-key-name";
-    const VALUE: &str = "hello world";
-    const RUNTIME_ARG_NAME: &str = "message";
-    const CONTRACT_WASM: &str = "contract.wasm";
+    use casper_engine_test_support::{Code, SessionBuilder, TestContextBuilder};
+    use casper_types::{runtime_args, AsymmetricType, ContractHash, PublicKey, RuntimeArgs, U512};
+
+    const ACCOUNT_A: [u8; 32] = [3u8; 32];
+    const ACCOUNT_B: [u8; 32] = [6u8; 32];
+    const ACCOUNT_C: [u8; 32] = [9u8; 32];
 
     #[test]
-    fn should_store_hello_world() {
-        let secret_key = SecretKey::ed25519_from_bytes(MY_ACCOUNT).unwrap();
-        let public_key = PublicKey::from(&secret_key);
-        let account_addr = AccountHash::from(&public_key);
+    fn test() {
+        // Prepare Account
+        let pub_a = PublicKey::ed25519_from_bytes(&ACCOUNT_A).unwrap();
+        let pub_b = PublicKey::ed25519_from_bytes(&ACCOUNT_B).unwrap();
+        let pub_c = PublicKey::ed25519_from_bytes(&ACCOUNT_C).unwrap();
 
+        let account_a = pub_a.to_account_hash();
+        let account_b = pub_b.to_account_hash();
+        let account_c = pub_c.to_account_hash();
+
+        // Prepare test context
         let mut context = TestContextBuilder::new()
-            .with_public_key(public_key, U512::from(500_000_000_000_000_000u64))
+            .with_public_key(pub_a, U512::from(100_000_000_000_000u64))
+            .with_public_key(pub_b, U512::from(100_000_000_000_000u64))
+            .with_public_key(pub_c, U512::from(100_000_000_000_000u64))
             .build();
 
-        // The test framework checks for compiled Wasm files in '<current working dir>/wasm'.  Paths
-        // relative to the current working dir (e.g. 'wasm/contract.wasm') can also be used, as can
-        // absolute paths.
-        let session_code = Code::from(CONTRACT_WASM);
-        let session_args = runtime_args! {
-            RUNTIME_ARG_NAME => VALUE,
+        println!("prepare finished");
+        // Deploy contract
+        let contract_code = Code::from("contract.wasm");
+        let create_args = runtime_args! {
+            "name" => String::from("Test DAO")
         };
-        let session = SessionBuilder::new(session_code, session_args)
-            .with_address(account_addr)
-            .with_authorization_keys(&[account_addr])
+        let create_session = SessionBuilder::new(contract_code, create_args)
+            .with_address(account_a)
+            .with_authorization_keys(&[account_a])
+            .build();
+        context.run(create_session);
+
+        // get contract hash
+        let hash: ContractHash = context
+            .query(account_a, &["DAO_contract_hash".into()])
+            .unwrap()
+            .into_t()
+            .unwrap();
+
+        assert_eq!(
+            context
+                .query(account_a, &["status".into()])
+                .unwrap()
+                .into_t::<String>()
+                .unwrap(),
+            "join".to_string()
+        );
+
+        // join
+        let join_code = Code::Hash(hash.value(), "join".into());
+        let join_b = SessionBuilder::new(join_code, runtime_args! {})
+            .with_address(account_b)
+            .with_authorization_keys(&[account_b])
             .build();
 
-        let result_of_query: Result<Value, Error> =
-            context.run(session).query(account_addr, &[KEY.to_string()]);
-
-        let returned_value = result_of_query.expect("should be a value");
-
-        let expected_value = Value::from_t(VALUE.to_string()).expect("should construct Value");
-        assert_eq!(expected_value, returned_value);
-    }
-
-    #[test]
-    #[should_panic(expected = "ApiError::MissingArgument")]
-    fn should_error_on_missing_runtime_arg() {
-        let secret_key = SecretKey::ed25519_from_bytes(MY_ACCOUNT).unwrap();
-        let public_key = PublicKey::from(&secret_key);
-        let account_addr = AccountHash::from(&public_key);
-
-        let mut context = TestContextBuilder::new()
-            .with_public_key(public_key, U512::from(500_000_000_000_000_000u64))
+        let join_code = Code::Hash(hash.value(), "join".into());
+        let join_c = SessionBuilder::new(join_code, runtime_args! {})
+            .with_address(account_c)
+            .with_authorization_keys(&[account_c])
             .build();
+        context.run(join_b);
+        context.run(join_c);
+        // get new hash
+        let hash: ContractHash = context
+            .query(account_a, &["DAO_contract_hash".into()])
+            .unwrap()
+            .into_t()
+            .unwrap();
 
-        let session_code = Code::from(CONTRACT_WASM);
-        let session_args = RuntimeArgs::new();
-        let session = SessionBuilder::new(session_code, session_args)
-            .with_address(account_addr)
-            .with_authorization_keys(&[account_addr])
-            .build();
-
-        context.run(session);
+        // proposal
+        let proposal_code = Code::Hash(hash.value(), "proposal".into());
+        let proposal = SessionBuilder::new(
+            proposal_code,
+            runtime_args! {"plan" => String::from("100000000")},
+        )
+        .with_address(account_a)
+        .with_authorization_keys(&[account_a])
+        .build();
+        context.run(proposal);
     }
 }
 
